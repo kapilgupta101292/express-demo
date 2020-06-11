fe01a82eca4766329cafef377eb2e03ad391e1ee
kapil.gupta <kapil.gupta@sirionlabs.com>
2020-06-11 11:09:04 +0530

    added config using config package

---
+++ index.js
@@ -0,0 +1,109 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const Joi = require('joi');
+const logger = require('./logger');
+const authenticator = require('./authenticator');
+const express = require('express');
+const app = express();
+
+console.log(`NODE_ENV : ${process.env.NODE_ENV}`);
+console.log(`app : ${app.get('env')}`);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if (app.get('env') === 'development') {
+  app.use(morgan('common'));
+  //   app.use(morgan('tiny'));
+  startupDebugger.log('Morgan Enabled');
+}
+
+app.use(logger);
+app.use(authenticator);
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello World !!!');
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('The course with the given id was not found');
+  }
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/course/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('The course with the given id was not found');
+  }
+  console.log('course ' + course);
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/course/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('The course with the given id was not found');
+  }
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(course, schema);
+  return result;
+}
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
